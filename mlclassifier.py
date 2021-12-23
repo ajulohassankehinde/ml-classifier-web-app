8b83dae71e5bf052c441eb0a75f5140f5aa8be8e
Hassan <ajulohassankehinde@gmail.com>
2021-12-23 10:45:28 +0200

    Update mlclassifier.py

--- mlclassifier.py
+++ mlclassifier.py
@@ -1,19 +1,3 @@
-import streamlit as st
-import numpy as np
-import seaborn as sns
-import pandas as pd
-
-import matplotlib.pyplot as plt
-from sklearn import datasets
-from sklearn.model_selection import train_test_split
-
-from sklearn.decomposition import PCA
-from sklearn.svm import SVC
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import RandomForestClassifier
-
-from sklearn.metrics import accuracy_score, confusion_matrix
-
 st.title("ML Classifier Web Application")
 
 dataset_name = st.sidebar.selectbox("Select Dataset", ("Iris", "Breast Cancer", "Wine"))
